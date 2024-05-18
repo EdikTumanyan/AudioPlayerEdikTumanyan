b06a9a0705bae007dfb4abc1f092c2cf80c87f4b
EdikTumanyan <edik.tumanyan@tumo.org>
2024-05-18 13:28:34 +0400

    final project

--- AudioPlayer_Edik-Tumanyan2/script.js
+++ AudioPlayer_Edik-Tumanyan2/script.js
@@ -1,27 +1,52 @@
 let data = {
-    title : [
+    title: [
         "Aria Math",
-        ""
+        "Gangsta's Paradise",
+        "I'm Still Standing",
+        "Believer",
+        "Little Dark Age",
+        "Billie Jean",
+        "Happy",
+        "Bohemian Rhapsody",
+        "Blinding Lights",
     ],
 
-    song : [
+    song: [
         "music/2-13. Aria Math.mp3",
-        "",
+        "music/Coolio - Gangsta's Paradise.mp3",
+        "music/Elton John – I'm Still Standing.mp3",
+        "music/Imagine Dragons - Believer.mp3",
+        "music/MGMT - Little Dark Age.mp3",
+        "music/Michael Jackson - Billie Jean.mp3",
+        "music/Pharrell Williams - Happy.mp3",
+        "music/Queen - Bohemian Rhapsody.mp3",
+        "music/The Weeknd - Blinding Lights.mp3"
     ],
 
-    poster : [
+    poster: [
         "https://media3.giphy.com/media/Wxc9iA6vBWMP9jbMjn/200w.gif?cid=6c09b952zt09oew5svjy3nehwfag00tnh6p6nw18n5acsbjb&ep=v1_gifs_search&rid=200w.gif&ct=g",
-        ""
+        "https://media3.giphy.com/media/Wxc9iA6vBWMP9jbMjn/200w.gif?cid=6c09b952zt09oew5svjy3nehwfag00tnh6p6nw18n5acsbjb&ep=v1_gifs_search&rid=200w.gif&ct=g",
+        "https://media3.giphy.com/media/Wxc9iA6vBWMP9jbMjn/200w.gif?cid=6c09b952zt09oew5svjy3nehwfag00tnh6p6nw18n5acsbjb&ep=v1_gifs_search&rid=200w.gif&ct=g",
+        "https://media3.giphy.com/media/Wxc9iA6vBWMP9jbMjn/200w.gif?cid=6c09b952zt09oew5svjy3nehwfag00tnh6p6nw18n5acsbjb&ep=v1_gifs_search&rid=200w.gif&ct=g",
+        "https://media3.giphy.com/media/Wxc9iA6vBWMP9jbMjn/200w.gif?cid=6c09b952zt09oew5svjy3nehwfag00tnh6p6nw18n5acsbjb&ep=v1_gifs_search&rid=200w.gif&ct=g",
+        "https://media3.giphy.com/media/Wxc9iA6vBWMP9jbMjn/200w.gif?cid=6c09b952zt09oew5svjy3nehwfag00tnh6p6nw18n5acsbjb&ep=v1_gifs_search&rid=200w.gif&ct=g",
+        "https://media3.giphy.com/media/Wxc9iA6vBWMP9jbMjn/200w.gif?cid=6c09b952zt09oew5svjy3nehwfag00tnh6p6nw18n5acsbjb&ep=v1_gifs_search&rid=200w.gif&ct=g",
+        "https://media3.giphy.com/media/Wxc9iA6vBWMP9jbMjn/200w.gif?cid=6c09b952zt09oew5svjy3nehwfag00tnh6p6nw18n5acsbjb&ep=v1_gifs_search&rid=200w.gif&ct=g",
+        "https://media3.giphy.com/media/Wxc9iA6vBWMP9jbMjn/200w.gif?cid=6c09b952zt09oew5svjy3nehwfag00tnh6p6nw18n5acsbjb&ep=v1_gifs_search&rid=200w.gif&ct=g",
+        "https://media3.giphy.com/media/Wxc9iA6vBWMP9jbMjn/200w.gif?cid=6c09b952zt09oew5svjy3nehwfag00tnh6p6nw18n5acsbjb&ep=v1_gifs_search&rid=200w.gif&ct=g",
+
     ]
 }
 
 
 let song = new Audio()
-window.onload = function(){
+window.onload = function () {
     playSong()
 }
 
 let currentSong = 0
+
+
 function playSong() {
     song.src = data.song[currentSong]
     let songTitle = document.getElementById("songTitle")
@@ -33,21 +58,107 @@ function playSong() {
     song.play()
 }
 
-function playOrPauseSong(){
+function playOrPauseSong() {
     let play = document.getElementById("play")
 
-    if(song.paused) {
+    if (song.paused) {
         song.play()
         play.src = "images/pause.png"
     } else {
         song.pause()
-        play.src = "images/pay-button-arrowhead.png"
+        play.src = "images/play-button-arrowhead.png"
     }
 }
 
-song.addEventListener("timeupdate" , function(){
+song.addEventListener("timeupdate", function () {
     let fill = document.getElementsByClassName("fill")
-    let position = song.currentTime / song.duration
 
-    fill[0].style.marginleft = position * 99 + "%"
-} )
+
+    let position = song.currentTime / song.duration
+    fill[0].style.marginLeft = position * 99 + "%"
+    convertTime(song.currentTime)
+
+    if (song.ended) {
+        next()
+    }
+})
+
+
+
+
+
+function convertTime(seconds) {
+    let currentTime = document.getElementsByClassName("currentTime")
+
+    let min = Math.floor(seconds / 60)
+    let sec = Math.floor(seconds % 60)
+
+    min = (min < 10) ? "0" + min : min
+    sec = (sec < 10) ? "0" + sec : sec
+
+
+    currentTime[0].textContent = min + ":" + sec
+
+    totalTime(Math.round(song.duration))
+
+}
+
+function totalTime(seconds) {
+    let currentTime = document.getElementsByClassName("currentTime")
+
+    let min = Math.floor(seconds / 60)
+    let sec = Math.floor(seconds % 60)
+
+    min = (min < 10) ? "0" + min : min
+    sec = (sec < 10) ? "0" + sec : sec
+
+    currentTime[0].textContent += " / " + min + ":" + sec
+}
+
+function prev() {
+    currentSong--
+
+    if (currentSong < 0) {
+        currentSong = data.song.length - 1
+    }
+
+    playSong()
+    play.src = "images/pause.png"
+}
+
+function next() {
+    currentSong++;
+
+    if (currentSong >= data.song.length) {
+        currentSong = 0
+    }
+
+    playSong();
+    play.src = "images/pause.png"
+}
+
+function mute() {
+    let mute = document.getElementById("mute")
+
+    if (song.muted) {
+        song.muted = false
+        mute.src = "images/volume.png"
+    } else {
+        song.muted = true
+        mute.src = "images/volume-mute.png"
+    }
+}
+
+function decrease(){
+    let mute = document.getElementById("mute")
+
+    song.volume -= 0.2
+
+    if(song.volume <= 0.1){
+        mute.src = "images/volume-mute.png"
+    }
+}
+
+function increase(){
+    song.volume += 0.2
+}
